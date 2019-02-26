a954a28ea9440da81ae77367dc920932f389fce8
Rushing <liming20110711@163.com>
2019-02-27 00:18:17 +0800

    update:20190227-01:增加自定义archetype

---
+++ helper-archetype/helper-archetype-server/src/main/java/com/coding/helper/helperarchetypeserver/HelperArchetypeServerApplication.java
@@ -0,0 +1,13 @@
+package com.coding.helper.helperarchetypeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelperArchetypeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelperArchetypeServerApplication.class, args);
+	}
+
+}
