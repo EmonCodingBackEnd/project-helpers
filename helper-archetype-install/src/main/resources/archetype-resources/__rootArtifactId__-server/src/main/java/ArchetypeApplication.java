07af29fa44f8f23c997912995007c0a30406d3a5
Rushing <liming20110711@163.com>
2019-03-01 01:32:59 +0800

    update:20190301-03

--- helper-archetype-install/src/main/resources/archetype-resources/__rootArtifactId__-server/src/main/java/ArchetypeApplication.java
+++ helper-archetype-install/src/main/resources/archetype-resources/__rootArtifactId__-server/src/main/java/ArchetypeApplication.java
@@ -5,7 +5,11 @@ package ${package};
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
 public class ArchetypeApplication {
 
