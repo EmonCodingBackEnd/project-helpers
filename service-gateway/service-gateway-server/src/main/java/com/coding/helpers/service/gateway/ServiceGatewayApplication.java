ac559cccc7282922af579851c3b07c7c7c0dc949
Rushing <liming20110711@163.com>
2019-04-17 22:23:01 +0800

    update:20190417-01

--- service-gateway/service-gateway-server/src/main/java/com/coding/helpers/service/gateway/ServiceGatewayApplication.java
+++ service-gateway/service-gateway-server/src/main/java/com/coding/helpers/service/gateway/ServiceGatewayApplication.java
@@ -2,12 +2,14 @@ package com.coding.helpers.service.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
-public class ServiceGatewayServerApplication {
+public class ServiceGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceGatewayServerApplication.class, args);
+		SpringApplication.run(ServiceGatewayApplication.class, args);
 	}
 
 }
--- service-gateway/service-gateway-server/src/test/java/com/coding/helpers/service/gateway/ServiceGatewayApplicationTests.java
+++ service-gateway/service-gateway-server/src/test/java/com/coding/helpers/service/gateway/ServiceGatewayApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ServiceGatewayServerApplicationTests {
+public class ServiceGatewayApplicationTests {
 
 	@Test
 	public void contextLoads() {
