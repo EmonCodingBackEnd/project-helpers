2a9fb6b596ff3622d4469bd3ee33a1df97478660
Rushing <liming20110711@163.com>
2019-05-26 13:13:34 +0800

    update:20190526-01

--- service-gateway/service-gateway-server/src/main/java/com/coding/helpers/service/gateway/ServiceGatewayApplication.java
+++ service-gateway/service-gateway-server/src/main/java/com/coding/helpers/service/gateway/ServiceGatewayApplication.java
@@ -2,8 +2,10 @@ package com.coding.helpers.service.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableDiscoveryClient
 @EnableZuulProxy
 @SpringBootApplication
 public class ServiceGatewayApplication {
