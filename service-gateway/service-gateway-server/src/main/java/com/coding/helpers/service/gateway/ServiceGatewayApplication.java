8e22affe19a6159922f15351e3bbbbe21badf025
Rushing <liming20110711@163.com>
2019-05-27 07:32:48 +0800

    update:20190527-01

--- service-gateway/service-gateway-server/src/main/java/com/coding/helpers/service/gateway/ServiceGatewayApplication.java
+++ service-gateway/service-gateway-server/src/main/java/com/coding/helpers/service/gateway/ServiceGatewayApplication.java
@@ -2,17 +2,25 @@ package com.coding.helpers.service.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 
 @EnableDiscoveryClient
 @EnableZuulProxy
 @SpringBootApplication
 public class ServiceGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceGatewayApplication.class, args);
+    }
 
+    /*// 动态路由
+    @RefreshScope
+    @ConfigurationProperties("zuul")
+    public ZuulProperties zuulProperties() {
+        return new ZuulProperties();
+    }*/
 }
-
