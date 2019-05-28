5868544f7a9a23b6f543d43b1dddcd5edd089c16
Rushing <liming20110711@163.com>
2019-05-29 07:50:57 +0800

    update:20190529-01

---
+++ helper-archetype/helper-archetype-server/src/main/java/com/coding/helper/ArchetypeApplication.java
@@ -0,0 +1,13 @@
+package com.coding.helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchetypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchetypeApplication.class, args);
+	}
+
+}
