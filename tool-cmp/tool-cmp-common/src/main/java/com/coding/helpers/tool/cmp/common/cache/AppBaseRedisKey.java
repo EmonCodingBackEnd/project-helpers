da9f1a76f08e14c1fc6640d4556219bfe0a9e9d3
Rushing <liming20110711@163.com>
2019-06-07 17:48:44 +0800

    update:20190607-01:调整缓存基类

--- tool-cmp/tool-cmp-common/src/main/java/com/coding/helpers/tool/cmp/common/cache/AppBaseRedisKey.java
+++ tool-cmp/tool-cmp-common/src/main/java/com/coding/helpers/tool/cmp/common/cache/AppBaseRedisKey.java
@@ -20,8 +20,30 @@ import java.util.List;
 
 public interface AppBaseRedisKey {
 
-    /** 非动态RedisKey获取方法. */
+    /**
+     * 获取key，不包含变量部分，适用于声明使用，比如 @Cacheable.
+     *
+     * <p>创建时间: <font style="color:#00FFFF">20190603 10:51</font><br>
+     * 比如： prefix:keyPart1:keyPart2......:
+     *
+     * @return java.lang.String
+     * @author Rushing0711
+     * @since 1.0.0
+     */
     String getKey();
+
+    /**
+     * 获取key，包含变量部分，适用于编程使用.
+     *
+     * <p>创建时间: <font style="color:#00FFFF">20190603 10:52</font><br>
+     * 比如： prefix:keyPart1:keyPart2......:keyVar
+     *
+     * @param keyVar - key中的变量部分
+     * @return java.lang.String
+     * @author Rushing0711
+     * @since 1.0.0
+     */
+    String getKey(String keyVar);
 
     /** Redis的Key前缀. */
     String getPrefix();
@@ -34,7 +56,8 @@ public interface AppBaseRedisKey {
      * 实现该接口的枚举可以在调用该方法.
      *
      * <p>创建时间: <font style="color:#00FFFF">20190327 19:10</font><br>
-     * [请在此输入功能详述]
+     * 比如： endWithColon = true, prefix:keyPart1:keyPart2......:<br>
+     * 比如： endWithColon = false, prefix:keyPart1:keyPart2......:keyVar
      *
      * @param endWithColon - 是否以冒号结尾
      * @param keyParts - 组成RedisKey的元素集合，不包含前缀，前缀是自动添加的
@@ -42,7 +65,7 @@ public interface AppBaseRedisKey {
      * @author Rushing0711
      * @since 1.0.0
      */
-    default String combineKeyParts(boolean endWithColon, Object... keyParts) {
+    default String combineKeyParts(boolean endWithColon, String... keyParts) {
         List<Object> keyList = new ArrayList<>();
         keyList.add(getPrefix());
         keyList.addAll(Arrays.asList(keyParts));
