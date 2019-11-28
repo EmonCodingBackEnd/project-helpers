ee6cd7e9218b1f165286ee4b4d83196dbb28f8c6
Rushing <liming20110711@163.com>
2019-11-29 07:54:14 +0800

    update:20191129-01

---
+++ service-gateway/service-gateway-server/src/main/java/com/coding/helpers/service/gateway/filter/PreRequestFilter.java
@@ -0,0 +1,30 @@
+package com.coding.helpers.service.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+/**
+ * 在过滤器中存储客户端发起请求的时间戳.
+ *
+ * <p>创建时间: <font style="color:#00FFFF">20191129 00:03</font><br>
+ * [请在此输入功能详述]
+ *
+ * @author Rushing0711
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@Slf4j
+@Component
+public class PreRequestFilter extends AbstractPreZuulFilter {
+    @Override
+    protected Object cRun() {
+        context.set("startTime", System.currentTimeMillis());
+        return success();
+    }
+
+    @Override
+    public int filterOrder() {
+        return FilterConstants.PRE_DECORATION_FILTER_ORDER - 3;
+    }
+}
