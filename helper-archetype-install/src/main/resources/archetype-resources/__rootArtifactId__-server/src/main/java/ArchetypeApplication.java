64b824385e0892c06d90b570d85ab16b49b4d6ed
Rushing <mingli@ishanshan.com>
2019-12-10 19:32:47 +0800

    update:20191210-04

--- helper-archetype-install/src/main/resources/archetype-resources/__rootArtifactId__-server/src/main/java/ArchetypeApplication.java
+++ helper-archetype-install/src/main/resources/archetype-resources/__rootArtifactId__-server/src/main/java/ArchetypeApplication.java
@@ -5,12 +5,17 @@ package ${package};
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+// @EnableScheduling
+// @EnableAsync
+// @EnableEurekaClient
 @SpringBootApplication
 public class ArchetypeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ArchetypeApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ArchetypeApplication.class, args);
+    }
 }
