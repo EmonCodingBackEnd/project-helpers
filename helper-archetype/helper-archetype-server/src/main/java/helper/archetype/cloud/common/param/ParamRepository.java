8fac11b2f5f1e1e7d6750e49e7e54cc66cb39378
Rushing <liming20110711@163.com>
2019-12-21 12:06:00 +0800

    update:20191221-04

--- helper-archetype/helper-archetype-server/src/main/java/helper/archetype/cloud/common/param/ParamRepository.java
+++
@@ -1,8 +0,0 @@
-package helper.archetype.cloud.common.param;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ParamRepository extends JpaRepository<Param, Long> {
-
-    Param findByParamCodeAndDeleted(String paramCode, Integer deleted);
-}
